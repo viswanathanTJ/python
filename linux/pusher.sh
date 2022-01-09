03f52778316933ffdcbfe3baa3ec7512a432ec63
viswanathanTJ <30386026+viswanathanTJ@users.noreply.github.com>
2022-01-09 17:44:57 +0530

    pusher

---
+++ linux/pusher.sh
@@ -0,0 +1,12 @@
+cd /mnt/SSD/Notepad/notepad
+git add .
+git commit -m "$(date +"%D %T")"
+GIT_SSH_COMMAND='ssh -i /mnt/SSD/Notepad/.ssh/id_ed25519 -o IdentitiesOnly=yes' git push -u origin master
+echo "Script successful"
+# -----BEGIN OPENSSH PRIVATE KEY-----
+#b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+#QyNTUxOQAAACBFcCMqWZXQeoSKwC+AN5FE65xGLpC6UrmuiGa9abryOAAAAJiZxpQwmcaU
+#MAAAAAtzc2gtZWQyNTUxOQAAACBFcCMqWZXQeoSKwC+AN5FE65xGLpC6UrmuiGa9abryOA
+#AAAEADGVgK/LB8NlHTo26wlJHXVnuDE0uFyst7US76vOON6kVwIypZldB6hIrAL4A3kUTr
+#nEYukLpSua6IZr1puvI4AAAAE3Zpc3dhMms1OUBnbWFpbC5jb20BAg==
+#-----END OPENSSH PRIVATE KEY-----
